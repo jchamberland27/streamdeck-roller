8bdd098d50d43419fd24da4170cffc98d68a4d1f
Josh <jchamberland27@gmail.com>
2023-11-17 21:12:24 +0000

    parameterize host port in build and deploy script

--- docker/docker_build_and_deploy.sh
+++ docker/docker_build_and_deploy.sh
@@ -1,13 +1,20 @@
 # utility script to build docker image
 echo "Build and deploy streamdeck-roller docker image..."
 
+#set $1 to default port if not passed in
+if [ -z "$1" ]
+  then
+    echo "No port argument supplied, using default port 5000"
+    set -- "5000"
+fi
+
 # move dockerfile into place
 cp dockerfile ../src/
 cd ../src
 
-# build and run docker image
+# build and run docker image on port passed into script
 docker image build -t streamdeck-roller .
-docker run -d -p 5000:5000 streamdeck-roller
+docker run -d -p $1:5000 streamdeck-roller
 
 #cleanup
 rm dockerfile
